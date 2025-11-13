61293fd71dcbaccf2463b629709d0d400b85f2dd
WildanYR <wildanyahya99@gmail.com>
2025-11-13 13:46:22 +0700

    fix github pages base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/fine-tuning-hexed/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
